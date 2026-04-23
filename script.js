43d46748d7876197d2303b2080b7f1b91bea117b
abduraimovxasan08-bit <abduraimovxasan08@gmail.com>
2026-04-23 19:18:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// let n = Number(prompt("n="))
+// for(let i=1; i<=10; i++){
+//     console.log(i*n);
+// }
+
+// let a = Number(prompt("a="))
+// if(a%2==0){
+//   console.log("juft");
+//   }
+// else{
+//     console.log("toq")
+// }
+
+// let n = Number(prompt("n="))
+// let s=0
+// for(let i=1; i<=n; i++){
+//     s+=i;
+//     console.log("yig'indi:",s);
+//  }
+
+// let a = Number(prompt("a="));
+// let b = Number(prompt("b="));
+//  if (a > b) {
+//      console.log("kattasi: " + a);
+//  } else if (b > a) {
+//     console.log("kattasi: " + b);
+//  } else {
+//     console.log(" teng");
+// }
+
+//  let yosh = Number(prompt("yosh="));
+//  let yil=2026
+//  console.log(yil-yosh)
+
+//   let a = Number(prompt("a="));
+//  let b = Number(prompt("b="));
+//  let c = Number(prompt("C="));
+//   if (a > b&&a>c) {
+//      console.log("kattasi: " + a);
+//  } else if (b > a&&b>c) {
+//      console.log("kattasi: " + b);
+//  } else if (c > a&&c>b) {
+//     console.log("kattasi: " + c);
+//  }  else {
+//      console.log(" teng");
+//   }
