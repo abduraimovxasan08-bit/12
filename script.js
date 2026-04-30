b47c5cf7cd44dffe56cf21c5311c374d8d3dbf29
abduraimovxasan08-bit <abduraimovxasan08@gmail.com>
2026-04-30 19:02:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
- let n = Number(prompt("n="))
- for(let i=1; i<=10; i++){
-     console.log(i*n);
+ let a = prompt("a kiriting");
+ if (a > 10){
+     console.log("a katta 10dan");
+ }else{
+     console.log("a kichik 10dan")
  }
 
- let a = Number(prompt("a="))
- if(a%2==0){
-   console.log("juft");
-   }
-else{
-    console.log("toq")
+
+ let a1 = prompt("a1 ni kiriting:");
+ let b1 = prompt("b1 ni kiriting:");
+ if (a1 == b1){
+     console.log("teng sonlar");
  }
 
-let n1 = Number(prompt("n="))
- let s=0
- for(let i=1; i<=n; i++){
-    s+=i;
-     console.log("yig'indi:",s);
-  }
 
- let a1 = Number(prompt("a="));
-let b1 = Number(prompt("b="));
-  if (a > b) {
-      console.log("kattasi: " + a);
- } else if (b > a) {
-    console.log("kattasi: " + b);
-  } else {
-     console.log(" teng");
+ let a2 = prompt("sonni kiriting:");
+ if (a2 % 5 == 0){
+     console.log ("5 ga bo'linadi");
+ }else{
+     console.log("5 ga bolinmaydi")
  }
 
-  let yosh = Number(prompt("yosh="));
-  let yil=2026
- console.log(yil-yosh)
 
-  let a2 = Number(prompt("a="));
-  let b = Number(prompt("b="));
-  let c = Number(prompt("C="));
-   if (a > b&&a>c) {
-      console.log("kattasi: " + a);
-  } else if (b > a&&b>c) {
-      console.log("kattasi: " + b);
-  } else if (c > a&&c>b) {
-     console.log("kattasi: " + c);
-  }  else {
-     console.log(" teng");
-   }
+  let a3 = Number(prompt("1-sonni kiriting:"));
+  let b3 = Number(prompt("2-sonni kiriting:"));
+  let c3 = Number(prompt("3-sonni kiriting:"));
+  if (a3 > b3 && a3 > c3){
+  console.log("max a3")}
+  else if  (b3 > a3 && b3 > c3){
+  console.log("max b3")}
+  else{
+  console.log("max c3")}
+
+
+
+ let harorat = prompt("haroratni kiriting:");
+ if ( harorat < 0){
+     console.log("muzlaydi");
+ }else{
+     console.log("oddiy holat");
+ }
+
+
+  for ( let i = 1; i <= 30; i++){
+    console.log(i);
+ }
+
+
+ for ( let i1 = 1; i1 <= 50; i1++){
+     if (i % 2 === 0){
+         console.log(i)
+     }
+ }
